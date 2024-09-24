692138dfc566598d044cf01990e0fd0c9db5a8f4
Nate <njgell7@gmail.com>
2024-09-24 14:25:32 +1000

    Ship genres with app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -15,9 +15,8 @@ interface Props {
 }
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
-  const { data, isLoading } = useGenres();
+  const { data, isLoading, error } = useGenres();
 
-  if (isLoading) return <Spinner />;
 
   return (
     <>
